d5063767d0b57a485df384fa7e7f11a9ba7bb441
Omanshuaman <omanshuaman@gmail.com>
2025-06-10 22:16:35 +0530

    Remove NotFoundScreen and HomePage components; update RootLayout to eliminate unused routes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -26,9 +26,6 @@ export default function RootLayout() {
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-
-        <Stack.Screen name="+not-found" />
       </Stack>
 
       <StatusBar style={"dark"} />
