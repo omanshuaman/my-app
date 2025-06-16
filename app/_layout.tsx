e6b11ae7121c7752cec93f3a5cf4924b61dea43a
Omanshuaman <omanshuaman@gmail.com>
2025-06-16 16:10:01 +0530

    initial commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,22 +1,5 @@
-import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
-import { StatusBar } from "expo-status-bar";
 
 export default function RootLayout() {
-  const [loaded] = useFonts({
-    PoppinsMedium: require("../assets/fonts/Poppins-Medium.ttf"),
-    PoppinsRegular: require("../assets/fonts/Poppins-Regular.ttf"),
-  });
-  if (!loaded) {
-    // Async font loading only occurs in development.
-    return null;
-  }
-  return (
-    <>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      </Stack>
-      <StatusBar hidden />
-    </>
-  );
+  return <Stack />;
 }
